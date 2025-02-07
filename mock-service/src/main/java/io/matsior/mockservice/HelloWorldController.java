04703b10ecfe58d725119c650be28da5b3e1a732
Mateusz Krajewski <maatsior@gmail.com>
2025-02-07 19:06:28 +0100

    Initial commit

---
+++ mock-service/src/main/java/io/matsior/mockservice/HelloWorldController.java
@@ -0,0 +1,16 @@
+package io.matsior.mockservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+class HelloWorldController {
+  
+  @GetMapping("/hello")
+  String helloWorld() {
+    return "Hello World! " + OffsetDateTime.now();
+  }
+  
+}
