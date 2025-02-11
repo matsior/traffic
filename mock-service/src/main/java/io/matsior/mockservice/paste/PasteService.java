8ef1213f75b92daa149b5142d2ad6d1c156fb116
Mateusz Krajewski <maatsior@gmail.com>
2025-02-11 18:32:32 +0100

    Move upload path to properties

--- mock-service/src/main/java/io/matsior/mockservice/paste/PasteService.java
+++ mock-service/src/main/java/io/matsior/mockservice/paste/PasteService.java
@@ -2,12 +2,12 @@ package io.matsior.mockservice.paste;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 
@@ -16,19 +16,27 @@ class PasteService {
   
   private static final Logger log = LoggerFactory.getLogger(PasteService.class);
   
+  private final Path uploadPath;
+  
+  PasteService(@Value("${mock.upload-path}") Path uploadPath) throws IOException {
+    this.uploadPath = uploadPath;
+    log.info("Uploading files to: {}", uploadPath);
+  }
+  
+  
   public List<String> getAll() {
     try {
-      List<String> paste = Files.readAllLines(Paths.get("./paste.txt"));
+      List<String> paste = Files.readAllLines(uploadPath);
       log.info("Returning {} paste", paste.size());
       return paste;
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
-  
+
   public void add(String content) {
     try {
-      Files.write(Path.of("paste.txt"), List.of(content), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+      Files.write(uploadPath, List.of(content), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
