0e49cfa6e4c60525dacfce72b202c24815c439dc
Mateusz Krajewski <maatsior@gmail.com>
2025-02-13 19:24:06 +0100

    Add environment variable

--- mock-service/src/main/java/io/matsior/mockservice/MockServiceApplication.java
+++ mock-service/src/main/java/io/matsior/mockservice/MockServiceApplication.java
@@ -1,13 +1,25 @@
 package io.matsior.mockservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MockServiceApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(MockServiceApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MockServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner commandLineRunner(@Value("${mock.test-value}") String testValue) {
+		return args -> log.info("Test value: {}", testValue);
 	}
 
 }
